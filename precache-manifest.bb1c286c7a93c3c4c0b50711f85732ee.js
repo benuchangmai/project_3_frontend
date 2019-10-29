dfd9bb7bd7e645fce51454504d5ed70648151ad2
benu <benuchangmai@gmail.com>
2019-10-28 21:58:34 -0700

    Updates

---
+++ precache-manifest.bb1c286c7a93c3c4c0b50711f85732ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff8e995660d7d63efc1f789cece4844",
+    "url": "/benuchangmai/index.html"
+  },
+  {
+    "revision": "52c63e8a39b2cf38fbbd",
+    "url": "/benuchangmai/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "813eb763244f79a0b5a6",
+    "url": "/benuchangmai/static/js/2.161bd094.chunk.js"
+  },
+  {
+    "revision": "52c63e8a39b2cf38fbbd",
+    "url": "/benuchangmai/static/js/main.bcf5d19c.chunk.js"
+  },
+  {
+    "revision": "8505629c78435f17c0f8",
+    "url": "/benuchangmai/static/js/runtime-main.93bbca39.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/benuchangmai/static/media/logo.25bf045c.svg"
+  }
+]);
